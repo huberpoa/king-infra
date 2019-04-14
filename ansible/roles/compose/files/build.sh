f643449d1f3fcb1f71fa406961b328ff420867a6
rfranzen <rfranzen@gmail.com>
2019-04-14 15:32:28 -0300

    using webhooks to trigger rebuild proccess

--- ansible/roles/compose/files/build.sh
+++ ansible/roles/compose/files/build.sh
@@ -1,23 +1,19 @@
 #!/usr/bin/env bash
 
 ##------------------------------------------------------------------------------
-## Script used for gitlab-ci to rebuild the environment when a new version is
-## available
+## Script used for gitlab-ci to rebuild the environment when a new commit is
+## made on the branch master
 ## --
-## Run this script with the version of the application then it will rebuild the
-## docker environment and restart everything.
-## --
-## If no parameter is entered, the script will only restart the docker
-## environment
+## This will rebuild the docker environment and restart everything.
 ##------------------------------------------------------------------------------
 
+cd /opt/application
+git reset --hard
+git pull
+
 cd /opt/docker
+echo "VERSION=$(date +\%Y-\%m-\%d_\%T)" > .env
 
-if [[ $1 ]]; then
-  echo "VERSION=$1" > .env
-
-  docker-compose build
-  docker-compose restart
-else
-  docker-compose restart
-fi
+docker-compose build
+docker-compose stop
+docker-compose up -d
