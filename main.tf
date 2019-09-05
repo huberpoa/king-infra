0307ef835094cc46d2049203c7c1f0dfa0bfc4c7
William Souza <william.souza@kinghost.com.br>
2019-09-05 07:08:43 -0300

    criado estrutura para criar instance no gcp

--- main.tf
+++ main.tf
@@ -3,3 +3,39 @@ provider "google" {
     region = "${var.region}"
     project = "${var.name-project}"
 }
+
+resource "google_compute_instance" "vm_instance" {
+    count = 1
+    name = "${var.names}-${count.index + 1}"
+    machine_type = "${var.machine}"
+    zone = "${var.region}-a"
+
+    metadata = {
+        sshKeys = "${var.user}:${file("${var.pub-key}")}"
+    }
+
+    boot_disk {
+        initialize_params {
+            image = "${var.image}"
+        }
+    }
+
+    network_interface {
+        network = "default"
+        access_config {}
+    }
+
+    provisioner "local-exec" {
+        command = "echo ${google_compute_instance.vm_instance[count.index].network_interface.access_config.0.nat_ip} >> host"
+    }
+}
+
+output "private_ip" {
+  value = "${google_compute_instance.vm_instance.*.network_interface.0.address}"
+}
+
+output "public_ip" {
+  value = "${google_compute_instance.vm_instance.*.network_interface.0.access_config.0.nat_ip}"
+}
+
+
--- variables.tf
+++ variables.tf
@@ -1,8 +1,23 @@
 variable "region" {
-    default = "us-east1"
+    default = "southamerica-east1"
 }
 
-variable "name-project" {
-    default = "infra"
+variable "name-project" {}
+
+variable "names" {
+  default = "web-server"
 }
 
+variable "machine" {
+    default = "n1-standard-2"
+}
+
+variable "mypath" {}
+
+variable "image" {
+    default = "centos-7"
+}
+
+variable "user" {}
+
+variable "pub-key" {}
