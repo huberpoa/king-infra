c9db4445a03711fc8c4f2047e66e2dbab9a7aa83
willalbiero <albiero.william@gmail.com>
2019-09-09 03:21:20 -0300

    Adicionado output para o nome dos servidores

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,8 +39,13 @@ resource "google_compute_instance" "vm_instance" {
     provisioner "local-exec" {
         command = "echo '[${var.env[count.index]}]\n${self.network_interface.0.access_config.0.nat_ip}  'ansible_ssh_user=$USER >> hosts"
     }
+
+}
+
+output "name_servers" {
+  value = "${google_compute_instance.vm_instance.*.name}"
 }
 
 output "public_ip" {
-  value = "${google_compute_instance.vm_instance.name} ${google_compute_instance.vm_instance.*.network_interface.0.access_config.0.nat_ip}"
+  value = "${google_compute_instance.vm_instance.*.network_interface.0.access_config.0.nat_ip}"
 }
